1bcca291b0e59496cbbebdb023d3e1284a82a3d3
aLodos3 <adrianlodostrinidad2@gmail.com>
2023-11-14 13:07:10 +0100

    commit

--- EjerciciosCode/archivos1.php
+++ EjerciciosCode/archivos1.php
@@ -4,6 +4,7 @@
     <title>Selector de Archivos</title>
 </head>
 <body>
+
 <form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post" enctype="multipart/form-data">
      <input type="color" name="color"><br/>
 	 <label for="archivo">Selecciona un archivo:</label>
@@ -13,7 +14,44 @@
 </form>
 <?php
 echo "<hr/><pre>";
-var_dump($_FILES);
+const MAX_FILE = 1048576;
+const TIPO_MIME = "text/plain";
+
+if (!empty($_FILES)) {
+	$tamanio_archivo = $_FILES["archivo"]["size"];
+	$nombre_archivo = $_FILES["archivo"]["tmp_name"];
+	$tipo_archivo = $_FILES["archivo"]["type"];
+	
+	if (($tamanio_archivo<=0) || ($tamanio_archivo>MAX_FILE)) {
+		$errores[] = "Tamaño de archivo incorrecto";
+	}
+	
+	if (($tipo_archivo!=TIPO_MIME)) {
+		$errores[] = "Formato de archivo incorrecto";
+	}
+	
+	if (empty($errores)) {
+		$color = (empty($_POST["color"])?"#0000FF":htmlspecialchars(trim($_POST["color"]))); 
+		echo "<p style='color:$color;'>";
+		
+	//	echo file_get_contents($nombre_archivo);
+		
+		$fichero = fopen($nombre_archivo,"r");
+		while (!feof($fichero)) {
+			echo fgets($fichero);
+		}
+		fclose($fichero);
+		
+		echo "</p>";
+	} else {
+		echo "<p style='color:red;'>";
+		foreach ($errores as $e) {
+			echo $e;
+		}
+		echo "</p>";
+	}
+	
+}
 echo "</pre>";
 ?>
 </body>

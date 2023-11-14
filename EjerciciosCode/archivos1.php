52f329c583e8b2c2c73addd5a279f64a80dddc55
aLodos3 <adrianlodostrinidad2@gmail.com>
2023-11-14 12:59:59 +0100

    commit

--- EjerciciosCode/archivos1.php
+++ EjerciciosCode/archivos1.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Selector de Archivos</title>
 </head>
 <body>
-    <?php
-        <form action="" method="post">
-        
-        </form>
-    ?>
+<form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post" enctype="multipart/form-data">
+     <input type="color" name="color"><br/>
+	 <label for="archivo">Selecciona un archivo:</label>
+	 <input type="file" name="archivo" id="archivo" accept=".txt"><br/>
+    <br>
+    <input type="submit" value="Subir">
+</form>
+<?php
+echo "<hr/><pre>";
+var_dump($_FILES);
+echo "</pre>";
+?>
 </body>
 </html>
